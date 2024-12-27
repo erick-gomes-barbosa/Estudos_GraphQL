c24d3ac90e1b7ce077d51a5bbbab72c1a52244fb
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2024-12-27 18:07:55 -0300

    Organização do código

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const {ApolloServer} = require("apollo-server")
-const { typeDefs, resolvers } = require("./src/graphql")
+const { schema } = require("./src/graphql")
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-})
+const server = new ApolloServer({ schema })
 
 server.listen().then(({ url}) => console.log(url));
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,13 @@
 const {join} = require("path");
-const { loadFilesSync, mergeTypeDefs, mergeResolvers, } = require("graphql-tools");
+const { makeExecutableSchema  }  = require("@graphql-tools/schema");
+const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
+const { loadFilesSync } = require("@graphql-tools/load-files");
 
 const allTypes = loadFilesSync(join(__dirname, "modules", "**", "*.gql"));
 const allResolvers = loadFilesSync(join(__dirname, "modules", "**", "resolvers.js"));
 
 const typeDefs = mergeTypeDefs(allTypes);
 const resolvers = mergeResolvers(allResolvers);
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
-module.exports = { typeDefs, resolvers };
+module.exports = { schema };
