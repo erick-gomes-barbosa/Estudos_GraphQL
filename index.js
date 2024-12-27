b56a32114251ea7b90f8f59b6c63bcf357b84d60
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2024-12-27 08:01:30 -0300

    Criação de segundo código exemplo e separação de código - Sujeito a correções

--- index.js
+++ index.js
@@ -1,81 +1,9 @@
-const {gql, ApolloServer} = require("apollo-server")
-
-const produtos = [
-    {
-        id: 1,
-        nome: 'tv',
-        valor: 2500.00
-    },
-    {
-        id: 2,
-        nome: 'pc',
-        valor: 4000.00
-    }
-]
-
-const usuarios = [
-    {
-        id: 1,
-        nome: 'erick',
-        idade: 19,
-        salario: 1234.34,
-        ativo: true 
-    },
-    {
-        id: 2,
-        nome: 'naju',
-        idade: 19,
-        salario: 3500.00,
-        ativo: true
-    }
-]
-
-const typeDefs = gql`
-    type Usuario{
-    idade: Int
-    salario: Float
-    nome: String
-    ativo: Boolean
-    id: ID
-    }
-
-    type Produto{
-        id: ID
-        nome: String
-        valor: Float
-    }
-
-    type Query{
-        usuarios: [Usuario]
-        produtos: [Produto]
-        usuario(id: Int, nome: String): Usuario
-        produto(id: Int, nome: String): Produto
-    }
-`;
-const resolvers = {
-    Query: {
-        usuarios(){
-            return usuarios;
-        },        
-        usuario(_, args){
-            const {id, nome} = args
-            if(id) return usuarios.find((usuario) => usuario.id == args.id);
-            return usuarios.find((usuario) => usuario.nome == args.nome);
-        },
-        produtos(){
-            return produtos;
-        },
-        produto(_, args){
-            const{id, nome} = args;
-            if(id) return produtos.find((produto) => produto.id == args.id);
-            return produtos.find((produto) => produto.nome == args.nome);
-        }
-    }
-}
+const {ApolloServer} = require("apollo-server")
+const { typeDefs, resolvers } = require("./src/graphql")
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
 })
 
-server.listen();
+server.listen().then(({ url}) => console.log(url));
---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    usuarios: [
+        {
+            id: 1,
+            nome: 'erick',
+            email: 'erick@email.com',
+            telefone: '11 91234 1234',
+            perfil: 1
+        },
+        {
+            id: 2,
+            nome: 'naju',
+            email: 'naju@email.com',
+            telefone: '11 94321 4321',
+            perfil: 2
+        }
+    ],
+    perfis: [
+        {id: 1, descricao: 'Administrador'},
+        {id: 2, descricao: 'Usuario_regular'},
+    ]
+}
